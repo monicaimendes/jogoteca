a2f98445916ea6eab499a1a93678476220271230
MonicaIacy <monicamendes81@yahoo.com>
2023-12-12 20:51:56 -0300

    add `novo.html` and bootstrap

--- jogoteca.py
+++ jogoteca.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 
 class Jogo:
@@ -8,18 +8,38 @@ class Jogo:
         self.console = console
 
 
+jogo1 = Jogo("Tetris", "Puzzle", "Atari")
+jogo2 = Jogo("Minecraft", "Platform", "XBOX")
+jogo3 = Jogo("It Takes Two", "Coop", "PS5")
+lista = [jogo1, jogo2, jogo3]
+
+
 app = Flask(__name__)
 
 
-@app.route("/inicio")
-def ola():
-    # jogos = ["Tetris", "Minecraft", "It Takes Two"]
-
-    jogo1 = Jogo("Tetris", "Puzzle", "Atari")
-    jogo2 = Jogo("Minecraft", "Platform", "XBOX")
-    jogo3 = Jogo("It Takes Two", "Coop", "PS5")
-    lista = [jogo1, jogo2, jogo3]
+@app.route("/")
+def index():
     return render_template("lista.html", titulo="Jogos", jogos=lista)
 
 
-app.run()
+@app.route("/novo")
+def novo():
+    return render_template("novo.html", titulo="Novo Jogo")
+
+
+@app.route(
+    "/criar",
+    methods=[
+        "POST",
+    ],
+)
+def criar():
+    nome = request.form["nome"]
+    categoria = request.form["categoria"]
+    console = request.form["console"]
+    jogo = Jogo(nome, categoria, console)
+    lista.append(jogo)
+    return redirect("/")
+
+
+app.run(debug=True)
